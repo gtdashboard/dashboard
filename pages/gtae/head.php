8216e2a34ec915b6be5e6259b516f062533a9172
Varsha <nilevars@gmail.com>
2017-10-04 16:29:57 +0300

    logo added

--- pages/gtae/head.php
+++ pages/gtae/head.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="icon" type="image/png" sizes="16x16" href="../arabi_logo_header.png">
   <title><?php echo $title;?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
